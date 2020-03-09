ea5378181102c811938a817eea719f8ed6ee1301
SoniaDumitru <soni.dumitru@gmail.com>
2020-03-09 16:00:14 -0500

    Update pathList.js

--- mountainbiker-client/src/containers/pathList.js
+++ mountainbiker-client/src/containers/pathList.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react'
 import PathCard from '../components/pathCard.js'
 import Path from '../components/path.js'
 import { NavLink } from 'react-router-dom'
-import style from '../styles/paths.css'
+import style from '../styles/style.css'
 
 export default class PathList extends Component {
-  render() {
 
+  render() {
       return(
         <div className="paths-list">
-        {this.props.paths.map(path =>
+        {this.props.paths ? (this.props.paths.map(path =>
           <NavLink to={`/path/${path.id }`}>
           <PathCard
             path={path}
             currentUser={this.props.currentUser}/>
-          </NavLink>)}
+          </NavLink>)) : (
+            <div className="loading-label">Loading paths...</div>
+          )}
         </div>
       )
     }
