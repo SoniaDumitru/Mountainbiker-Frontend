bbde4892a003ffe7160fde9bfa1205859b8a1783
SoniaDumitru <soni.dumitru@gmail.com>
2020-03-09 15:43:57 -0500

    Update header.js

--- mountainbiker-client/src/components/header.js
+++ mountainbiker-client/src/components/header.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Menu, Button, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { Menu, Segment } from 'semantic-ui-react'
+import { Navbar} from "react-bootstrap"
 import User from './user.js'
 
 export default class Header extends Component {
@@ -12,33 +13,43 @@ export default class Header extends Component {
   render() {
     const { activeItem } = this.state
     return(
-      <Segment inverted vertical >
-
+      <Segment inverted vertical>
         <Menu inverted pointing secondary>
+          <Navbar.Collapse className="welcome" collapseOnSelect>
+            <Link to="/home" className="item">Home</Link>
+          </Navbar.Collapse>
 
-        <Link to="/home" className="item">Home</Link>
-          <Link to="/paths" className="item">Paths</Link>
-         { !this.props.currentUser ?
-            <Menu.Menu position="right">
+          <Navbar.Collapse className="welcome">
+            <Link to="/paths" className="item">MTB Trails</Link>
+          </Navbar.Collapse>
+
+          { !this.props.currentUser ?
+          <Menu.Menu position="right">
+            <Navbar.Collapse className="welcome">
               <Link to="/signup" className="item">Sign Up</Link>
+            </Navbar.Collapse>
+
+            <Navbar.Collapse className="welcome">
               <Link to="/login" className="item">Log In</Link>
-             </Menu.Menu>
-               :
-            <Menu.Menu position="right">
-            <Menu.Item>{`Welcome, ${this.props.currentUser.name}!`}</Menu.Item>
+            </Navbar.Collapse>
+          </Menu.Menu>
+             :
+          <Menu.Menu position="right">
+            <Navbar.Collapse className="welcome">
+              <Menu.Item>{`Hi, ${this.props.currentUser.name}!`}</Menu.Item>
+            </Navbar.Collapse>
 
-            <Menu.Item onClick={this.props.logout}>
-              Log out
-            </Menu.Item>
-
-           </Menu.Menu>}
+            <Navbar.Collapse className="welcome">
+              <Menu.Item onClick={this.props.logout}>Log out</Menu.Item>
+            </Navbar.Collapse>
+         </Menu.Menu>}
 
          { !this.props.currentUser ? null :
-         <Link to="/profile" className="item">
-           My Profile
-       </Link> }
-      </Menu>
-     </Segment>
+           <Navbar.Collapse className="welcome">
+            <Link to="/profile" className="item">My Profile</Link>
+          </Navbar.Collapse> }
+        </Menu>
+      </Segment>
     )
   }
 }
