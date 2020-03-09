b09b602fc4961232cfeb9dd5b854c5a4db6851b9
SoniaDumitru <soni.dumitru@gmail.com>
2020-03-09 15:54:10 -0500

    Update searchPaths.js

--- mountainbiker-client/src/components/searchPaths.js
+++ mountainbiker-client/src/components/searchPaths.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
-import { Search, Grid, Header, Segment } from 'semantic-ui-react'
-import style from '../styles/paths.css'
+import style from '../styles/style.css'
 
-export default class SearchPath extends Component {
+class SearchPath extends Component {
+
   render() {
     return (
       <div className="search">
           <div className="ui big icon input">
             <input
-            size="30"
+              size="30"
               type="text"
               placeholder={"Search by name or location..."}
               onChange={event=> this.props.handleSearchChange(event)}
               value={this.props.search}/>
             <i className="circular search link icon"></i>
           </div>
-        </div>
+      </div>
     )
   }
 }
+
+export default SearchPath
