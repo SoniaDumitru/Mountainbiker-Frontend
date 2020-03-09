205fc8a4cd55938b48e6cf9face66b591a54619c
SoniaDumitru <soni.dumitru@gmail.com>
2020-03-09 15:55:27 -0500

    Update user.js

--- mountainbiker-client/src/components/user.js
+++ mountainbiker-client/src/components/user.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import style from '../styles/paths.css'
-import { Divider, Image, Segment, Button, Icon } from 'semantic-ui-react'
 import { NavLink } from "react-router-dom"
+import style from '../styles/style.css'
+import { Input, Icon,TextArea, Grid, Button, Dimmer, Header, Image, Segment, Form, Divider} from 'semantic-ui-react'
+
 
 export default class User extends Component {
   constructor(props) {
@@ -18,7 +19,7 @@ export default class User extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/v1/adventures/')
+    fetch('https://mountainbiker.herokuapp.com/api/v1/adventures/')
       .then(response => response.json())
       .then(adventures => this.setState({
         adventures: adventures.filter(adventure => adventure.user_id == this.props.currentUser.id)
@@ -26,7 +27,7 @@ export default class User extends Component {
   }
 
   deleteAdventures =(id)=> {
-    fetch(`http://localhost:3000/api/v1/adventures/${id}`, {
+    fetch(`https://mountainbiker.herokuapp.com/api/v1/adventures/${id}`, {
       method: 'DELETE',
       headers: {
         "Content-Type": "application/json",
@@ -46,7 +47,7 @@ export default class User extends Component {
      let id = userId
       id = `${this.props.currentUser.id}`
       console.log(id)
-       fetch(`http://localhost:3000/api/v1/users/${id}`, {
+       fetch(`https://mountainbiker.herokuapp.com/api/v1/users/${id}`, {
          method: 'DELETE',
          headers: {
            "Content-Type": "application/json",
@@ -61,36 +62,61 @@ export default class User extends Component {
       this.props.history.push('/login')
   }
 
-
   render() {
-
     let currentUser = this.props.currentUser
-    // console.log(currentUser)
-    return(
-      <div style={{marginTop: '60px', marginBottom: '25px'}}>
-        <Image rounded size="medium" className="ui medium centered image" src={currentUser.image}/><br></br>
-          <div className="username">{currentUser.name}</div><br></br>
-            <div><i>🏠 <strong>Location: {currentUser.location}</strong></i></div><br></br>
-              <div ><i><strong>Member since: {this.date(currentUser.created_at)}</strong></i></div><br></br>
-                <div><Button marginBottom="20px" inverted color="red" onClick={() => this.deleteCurrentUser(currentUser.id)}>Delete Account</Button></div><br></br>
+    return (
+    <Grid columns={2}>
+      <Segment style={{marginTop: '30px', marginBottom: '30px', width: '400px', marginLeft: '35px'}}>
+          <Image
+            rounded
+            size="medium"
+            className="ui medium centered image"
+            src={currentUser.image}>
+          </Image><br></br>
 
-                  <div><NavLink to='/edit' exact><Button marginBottom="20px" inverted color="green" >Update Account</Button></NavLink></div><br></br>
+        <div className="username">{currentUser.name}</div><br></br>
+        <div><i>🏠<strong>Location: {currentUser.location}</strong></i></div><br></br>
+        <div><i><strong>Member since: {this.date(currentUser.created_at)}</strong></i></div><br></br>
 
-                  {this.state.adventures.length > 0 ? <div className="username"> My Adventures </div> : null}<br></br>
-                  <div>< Icon name='trash' color = 'red'/>when clicked your adventure will be removed</div>
-                  {this.state.adventures.map(adventure =>
-                    <div onClick={() => this.deleteAdventures(adventure.id)} className="path-card-user">
-                      <img className="path-card-image-user" src={adventure.image}/>
-                        <div className="path-card-info-user">
-                          <div className='path-card-details-user'>{adventure.name}</div>
-                            <div className='path-card-details-user' >{adventure.location}</div>
-                          <div className='path-card-details-user' >{adventure.length} miles</div>
-                        <div  className='path-card-details-user' >{adventure.summary}</div>
+        <div>
+          <Button
+            marginBottom="20px"
+            inverted color="red"
+            onClick={() => this.deleteCurrentUser(currentUser.id)}>Delete Account
+          </Button>
+        </div><br></br>
+
+        <div>
+          <NavLink
+            to='/edit'
+            exact>
+          <Button
+            marginBottom="20px"
+            inverted color="green">Update Account</Button>
+          </NavLink>
+        </div><br></br>
+        </Segment>
+
+        <Segment style={{marginTop: '30px', marginBottom: '30px', width: '1000px', background: '#F8F8F8'}}>
+        {this.state.adventures.length > 0 ?
+        <div style={{fontSize: '30px'}}>My Adventures </div> : null}<br></br>
+            {this.state.adventures.map(adventure =>
+                  <div className="path-card-user">
+                      <img className="path-card-image" src={adventure.image}/>
+                      <div className="path-card-info-user">
+                      <div className='path-card-title-user'><strong>{adventure.name}</strong></div>
+                      <div className='path-card-location-user' >{adventure.location}</div>
+                      <div className='path-card-details-user' >{adventure.length} miles</div>
+                      <div  className='path-card-details-user' >{adventure.summary}</div>
+                      <button
+                        className="adventure-button-delete"
+                        onClick={() => this.deleteAdventures(adventure.id)}>
+                        <Icon> 🗑️</Icon>
+                      </button>
                   </div>
-              </div>)}
-          </div>
+          </div>)}
+          </Segment>
+      </Grid>
       )
     }
-  }
-
-  // <button className="trash-button" onClick={() => this.deleteAdventures(adventure.id)}><Icon name='trash' />delete</button><br></br>
+}
